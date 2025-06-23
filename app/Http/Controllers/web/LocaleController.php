0bcd2a37e21f34da5e38c7cbb1c324ffab8b581e
Ahmad Ghafeer <ahmadghafeer@gmail.com>
2025-06-23 16:18:17 +0300

    new

--- app/Http/Controllers/web/LocaleController.php
+++ app/Http/Controllers/web/LocaleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\web;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Redirect;
 
@@ -13,13 +14,12 @@ class LocaleController extends Controller
     
    public function setLocale(string $locale)
     {
-        dd($locale, config('app.available_locales')); // Debug line 1
         if (array_key_exists($locale, config('app.available_locales'))) {
             App::setLocale($locale);
             Session::put('locale', $locale);
-            dd(App::getLocale(), Session::get('locale')); // Debug line 2
         }
-        return Redirect::back();
+        Log::info('Locale changed to ' . $locale);
+        return Redirect::to(url()->previous());
     }
     
 }
